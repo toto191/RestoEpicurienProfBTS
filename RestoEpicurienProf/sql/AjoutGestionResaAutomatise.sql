a41d2d893d090731479c3ab4e6a63bf5a4c80ff4
toto191 <tombenoit612@gmail.com>
2026-06-02 12:31:44 +0200

    ajout schéma sql

--- RestoEpicurienProf/sql/AjoutGestionResaAutomatise.sql
+++ RestoEpicurienProf/sql/AjoutGestionResaAutomatise.sql
@@ -35,40 +35,6 @@ VALUES (2, 'midi', '12:00:00', '14:30:00'),
     (5, 'midi', '12:00:00', '14:30:00'),
     (5, 'soir', '19:30:00', '22:00:00'),
     (6, 'soir', '19:30:00', '22:30:00');
--- 3. Durées de service selon le nombre de couverts (inchangée)
-CREATE TABLE IF NOT EXISTS Durees_service (
-    id_duree INT PRIMARY KEY AUTO_INCREMENT,
-    nb_personnes_min TINYINT NOT NULL,
-    nb_personnes_max TINYINT NOT NULL,
-    duree_minutes SMALLINT NOT NULL,
-    CONSTRAINT chk_ordre CHECK (nb_personnes_min <= nb_personnes_max)
-);
-INSERT INTO Durees_service (
-        nb_personnes_min,
-        nb_personnes_max,
-        duree_minutes
-    )
-VALUES (2, 3, 105),
-    (4, 6, 150),
-    (7, 8, 180),
-    (9, 10, 210),
-    (11, 99, 240);
--- 4. Disponibilité par créneau + table + date
---    Reliée à Reservation_Tables pour savoir quelle table est physiquement occupée
-CREATE TABLE IF NOT EXISTS Dispo_Creneaux (
-    id_dispo INT PRIMARY KEY AUTO_INCREMENT,
-    id_creneau INT NOT NULL,
-    id_table INT NOT NULL,
-    date_dispo DATE NOT NULL,
-    est_disponible TINYINT(1) NOT NULL DEFAULT 1,
-    heure_fin_prevue TIME DEFAULT NULL,
-    id_reservation INT DEFAULT NULL,
-    UNIQUE KEY uq_dispo (id_creneau, id_table, date_dispo),
-    CONSTRAINT fk_dispo_creneau FOREIGN KEY (id_creneau) REFERENCES Creneaux(id_creneau),
-    CONSTRAINT fk_dispo_table FOREIGN KEY (id_table) REFERENCES Tables_Resto(id_table),
-    CONSTRAINT fk_dispo_reservation FOREIGN KEY (id_reservation) REFERENCES Reservations(id_reservation) ON DELETE
-    SET NULL
-);
 -- 5. Ajout de id_creneau sur Reservations (seule modification d'une table existante)
 ALTER TABLE Reservations
 ADD COLUMN id_creneau INT DEFAULT NULL
